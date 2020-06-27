7313a5ab22faba542b81fa9f401c3a06202d580d
Herverson de Sousa <herverson.sousa@gmail.com>
2020-06-27 19:36:39 -0300

    Feature: Add firebase and auth facade

---
+++ lib/domain/core/errors.dart
@@ -0,0 +1,16 @@
+import 'package:notes/domain/core/failures.dart';
+
+import 'failures.dart';
+
+class UnexpectedValueError extends Error {
+  final ValueFailure valueFailure;
+
+  UnexpectedValueError(this.valueFailure);
+
+  @override
+  String toString() {
+    const explanation =
+        'Encountered a ValueFailure at an unrecoverable point. Terminating.';
+    return Error.safeToString('$explanation Failure was: $valueFailure');
+  }
+}
