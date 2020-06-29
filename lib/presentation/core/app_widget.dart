850761b3c28d05dea9290a49fc4dafb7c762c8f1
Herverson de Sousa <herverson.sousa@gmail.com>
2020-06-29 08:51:30 -0300

    Feature: Splash screen

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -1,19 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/application/auth/auth_bloc.dart';
 import 'package:notes/presentation/sign_in/sign_in_page.dart';
+import 'package:notes/application/auth/auth_bloc.dart';
+import 'package:notes/presentation/routes/router.gr.dart';
+import '../../injection.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Notas',
-      home: SignInPage(),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        primaryColor: Colors.yellow,
-        accentColor: Colors.blueAccent,
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) =>
+              getIt<AuthBloc>()..add(const AuthEvent.authCheckRequested()),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Notas',
+        home: SignInPage(),
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.light().copyWith(
+          primaryColor: Colors.yellow,
+          accentColor: Colors.blueAccent,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
           ),
         ),
       ),
